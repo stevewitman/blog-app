ffc99eb917f2726f7696be004aefd058a6f3e969
Steve Witman <stevewitman@gmail.com>
2016-11-19 23:47:59 -0700

    Separate public and private pages/actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  get 'about' => 'pages#about', as: :about
-  get 'contact' => 'pages#contact', as: :contact
-  root to: 'posts#index'
-  resources :posts
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'blog/posts#index'
+
+  namespace :author do
+    resources :posts
+  end
+
+  scope module: 'blog' do
+    get 'about' => 'pages#about', as: :about
+    get 'contact' => 'pages#contact', as: :contact
+    get 'posts' => 'posts#index', as: :posts
+    get 'posts/:id' => 'posts#show', as: :post
+  end
 end
