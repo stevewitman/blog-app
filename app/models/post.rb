d0b2bc7a8315e0d98bfe477cda0180174a0661db
Steve Witman <stevewitman@gmail.com>
2016-11-19 20:54:46 -0700

    Refactor post index and show pages

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,15 @@ class Post < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
 
- def should_generate_new_friendly_id?
+  scope :most_recent, -> {order(id: :desc)}
+
+  def should_generate_new_friendly_id?
    title_changed?
- end
+  end
+
+  def display_day_published
+    "Published #{created_at.strftime('%-b %-d, %Y')}"
+  end
 
 
 end
