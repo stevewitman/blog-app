9818c241ea0c547e6585523413e37b103b0a1f30
Steve Witman <stevewitman@gmail.com>
2016-11-23 09:17:34 -0700

    Add tinymce editor

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,8 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+document.addEventListener("turbolinks:load", function() {
+  tinymce.remove();
+  tinymce.init({ selector:'textarea#post_body' });
+})
